63173b1a6effe6e29734a938639db24a90b5d32d
Martinabraham333 <martinmangalagiri@gmail.com>
2025-10-01 07:29:52 +0530

    first commit and created basic login page design

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:ayurvedic_centre_patients/presentation/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: LoginScreen()
+    );
+  }
+}
